aef8aff1c843ba90e1fad25cf78987438bdc6ff6
bruno.souza <bruno.souza@locaweb.com.br>
2019-08-23 13:59:42 -0300

    Initial commit

---
+++ lib/ex_correios.ex
@@ -0,0 +1,18 @@
+defmodule ExCorreios do
+  @moduledoc """
+  Documentation for ExCorreios.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExCorreios.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_correios_test.exs
@@ -0,0 +1,8 @@
+defmodule ExCorreiosTest do
+  use ExUnit.Case
+  doctest ExCorreios
+
+  test "greets the world" do
+    assert ExCorreios.hello() == :world
+  end
+end
