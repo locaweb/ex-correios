97fbaf3255c5cd4ce36dd4624f8725c27b3ed444
bruno.souza <bruno.souza@locaweb.com.br>
2019-08-28 15:24:19 -0300

    [correios-client] Fix convert string to float issue

--- lib/request/response.ex
+++ lib/request/response.ex
@@ -6,21 +6,6 @@ defmodule ExCorreios.Request.Response do
   import SweetXml, only: [transform_by: 2, sigil_x: 2, xpath: 3]
 
   @root_path '//Servicos/cServico'
-  @shipping_path [
-    deadline: ~x"//PrazoEntrega/text()"i,
-    declared_value: transform_by(~x"//ValorValorDeclarado/text()"s, &String.to_float/1),
-    error_code: ~x"//Erro/text()"s,
-    error_message: ~x"//MsgErro/text()"s,
-    home_delivery: ~x"//EntregaDomiciliar/text()"s,
-    manually_entered_value: transform_by(~x"//ValorMaoPropria/text()"s, &String.to_float/1),
-    notes: ~x"//obsFim/text()"s,
-    response_status: ~x"//Erro/text()"s,
-    receiving_alert_value: transform_by(~x"//ValorAvisoRecebimento/text()"s, &String.to_float/1),
-    saturday_delivery: ~x"//EntregaSabado/text()"s,
-    service_code: ~x"//Codigo/text()"s,
-    value: transform_by(~x"//Valor/text()"s, &String.to_float/1),
-    value_without_additionals: transform_by(~x"//ValorSemAdicionais/text()"s, &String.to_float/1)
-  ]
 
   @spec process(%HTTPotion.Response{}) :: {:ok, map()} | {:ok, list(struct())}
   def process(%HTTPotion.Response{status_code: 200, body: body}), do: {:ok, parser(body)}
@@ -29,9 +14,34 @@ defmodule ExCorreios.Request.Response do
   def process(%HTTPotion.ErrorResponse{message: reason}), do: {:error, reason}
 
   defp parser(body) do
-    case xpath(body, %SweetXpath{path: @root_path, is_list: true}, @shipping_path) do
+    case xpath(body, %SweetXpath{path: @root_path, is_list: true}, shipping_path()) do
       [value] -> value
       values -> values
     end
+  end
+
+  defp shipping_path do
+    [
+      deadline: ~x"//PrazoEntrega/text()"i,
+      declared_value: transform_by(~x"//ValorValorDeclarado/text()"s, &to_float/1),
+      error_code: ~x"//Erro/text()"s,
+      error_message: ~x"//MsgErro/text()"s,
+      home_delivery: ~x"//EntregaDomiciliar/text()"s,
+      manually_entered_value: transform_by(~x"//ValorMaoPropria/text()"s, &to_float/1),
+      notes: ~x"//obsFim/text()"s,
+      response_status: ~x"//Erro/text()"s,
+      receiving_alert_value: transform_by(~x"//ValorAvisoRecebimento/text()"s, &to_float/1),
+      saturday_delivery: ~x"//EntregaSabado/text()"s,
+      service_code: ~x"//Codigo/text()"s,
+      value: transform_by(~x"//Valor/text()"s, &to_float/1),
+      value_without_additionals: transform_by(~x"//ValorSemAdicionais/text()"s, &to_float/1)
+    ]
+  end
+
+  defp to_float(string) do
+    string
+    |> String.replace(",", ".")
+    |> Float.parse()
+    |> elem(0)
   end
 end
