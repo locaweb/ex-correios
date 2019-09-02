06aec7fb6cf9fb5060a835f27e59079ba1fe8932
bruno.souza <bruno.souza@locaweb.com.br>
2019-09-02 14:02:57 -0300

    [correios-client] Add log to all requests

--- lib/request/client.ex
+++ lib/request/client.ex
@@ -2,13 +2,21 @@ defmodule ExCorreios.Request.Client do
   @moduledoc """
   This module provides a http interface to communicate with Correios API.
   """
+  require Logger
 
   alias ExCorreios.Request.Response
 
   @spec get(String.t()) :: {:ok, map()} | {:ok, list(struct)} | {:error, String.t()}
   def get(url) do
     url
+    |> log_request()
     |> HTTPotion.get()
     |> Response.process()
+  end
+
+  defp log_request(url) do
+    Logger.info(fn -> "Processing with #{__MODULE__}\n  URL: #{url}" end)
+
+    url
   end
 end
